7db1a4ef43686e0d720f42a3539947d343709020
Benjamin Robinson <benjaminrobinson1313@gmail.com>
2023-11-15 21:14:08 +0800

    process the transactions amount on create

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -12,11 +12,21 @@ class Transaction < ApplicationRecord
   validates :amount, numericality: { greater_than: 0 }
   validate :sufficient_funds
 
+  # Callbacks
+  before_create :process_transaction
+
   private
 
   def sufficient_funds
     return if amount.blank? || from_account.blank?
 
     errors.add(:amount, 'exceeds available balance') if from_account.balance < amount
+  end
+
+  def process_transaction
+    ActiveRecord::Base.transaction do
+      from_account.update_column(:balance, from_account.balance - amount)
+      to_account.update_column(:balance, to_account.balance + amount)
+    end
   end
 end
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -41,4 +41,42 @@ RSpec.describe Transaction, type: :model do
       expect(described_class.reflect_on_association(:to_account).macro).to eq(:belongs_to)
     end
   end
+
+  describe '#process_transaction' do
+    subject(:transaction) { build(:transaction, from_account:, to_account:, amount:) }
+
+    context 'when the transaction is successfully created' do
+      let(:from_account) { create(:account, balance: 1000) }
+      let(:to_account) { create(:account, balance: 500) }
+      let(:amount) { 100 }
+
+      it 'deducts the amount from the from_account' do
+        transaction.save
+        expect(transaction.from_account.reload.balance.to_i).to eq(900)  # 1000 - 100
+      end
+
+      it 'adds the amount to the to_account' do
+        transaction.save
+        expect(transaction.to_account.reload.balance).to eq(600)  # 500 + 100
+      end
+    end
+
+    context 'when the transaction fails due to insufficient funds' do
+      let!(:from_account) { create(:account, balance: 50) }
+      let!(:to_account) { create(:account, balance: 500) }
+      let(:amount) { 100 }
+
+      it 'does not create a transaction' do
+        expect { transaction.save }.not_to change(Transaction, :count)
+      end
+
+      it 'does not change the balance of the from_account' do
+        expect { transaction.save }.not_to(change { from_account.reload.balance })
+      end
+
+      it 'does not change the balance of the to_account' do
+        expect { transaction.save }.not_to(change { to_account.reload.balance })
+      end
+    end
+  end
 end
